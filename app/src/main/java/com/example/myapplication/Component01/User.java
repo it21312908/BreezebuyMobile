4720f7a4d34d38d89b456d484c586b701304f320
IT21307058 <it21307058@my.sliit.lk>
2024-10-07 22:09:43 +0530

    User class added

---
+++ app/src/main/java/com/example/myapplication/Component01/User.java
@@ -0,0 +1,48 @@
+package com.example.myapplication.Component01;
+
+public class User {
+    private String username;
+    private String password;
+    private String status;
+    private String role;
+
+
+    public User(String username, String password, String status, String role) {
+        this.username = username;
+        this.password = password;
+        this.status = status;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
